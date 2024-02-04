9018c3143b4c04932b62eb1566ce0af7c4fb8ce2
Leonardo Zappi Ramos <leo.zappi@hotmail.com>
2024-02-04 11:57:19 -0300

    Setup Docker Compose and Env file

--- zappi_cake/settings.py
+++ zappi_cake/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# 'django-insecure-aa%&w1bfmt@304^9rbl_6-&h*y4(8kl+%nlff13emkrzy^y(xl'
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
